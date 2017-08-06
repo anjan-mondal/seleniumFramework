63f18e860af343ef0a5e9e6093ec2724494b955f
anjan <anjanmondal@gmail.com>
2017-08-07 01:42:21 +0530

    commit

--- src/testscripts/testcase/TC_02.java
+++ src/testscripts/testcase/TC_02.java
@@ -1,6 +1,5 @@
 package testscripts.testcase;
 
-import org.junit.Before;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
@@ -8,6 +7,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import components.common.Report;
 import components.common.Reporter;
 import components.common.Reuseable;
 import components.common.TestCase;
@@ -16,60 +16,53 @@ import components.pages.FBProfile;
 
 import org.testng.annotations.DataProvider;
 
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
-
 import utility.ExcelUtils1;
 
 public class TC_02 extends TestCase {
 
-	
 	Reuseable reuseable;
-	ExtentReports report;
-	ExtentTest logger;
-	String caseName ;
-	
+	// ExtentReports report;
+	// ExtentTest logger;
+	String caseName;
+	Report report;
+
 	@BeforeSuite
-	
-	public void setUp(){
-		caseName= (getTestSuiteName(this.toString()));
-		report = new ExtentReports("D:\\report\\"+caseName+".html",false);
-		
-		logger=report.startTest("Test case 2");
+
+	public void setUp() {
+		caseName = (getTestCaseName(this.toString()));
+		driver = getDriver("chrome");
+		report = new Report(driver);
+		report.setTestSuiteName(caseName);
+		report.setTestSummary("Open google page");
 	}
-	
+
 	@Test(dataProvider = "test")
 
-	public void executeTest(String Username, String password, String url,String broswer) {
-		
-		
-		driver = getDriver(broswer); 		
-		reuseable=new Reuseable(driver);
-		driver.manage().window().maximize();
-	    reuseable.LaunchApplication(url);
-	    logger.log(LogStatus.PASS, "URl launched" + url + "<a href ='#'/> click");
-		FBLogin login = new FBLogin(driver);
-		
-		//.login(Username, password);
-		
-		FBProfile profile = new FBProfile(driver);
-	    logger.log(LogStatus.FAIL, "Profile not loaded");
+	public void executeTest(String Username, String password, String url, String broswer) {
 
-		//profile.logout();
-	    Wrap();
+		reuseable = new Reuseable(driver);
+		driver.manage().window().maximize();
+		reuseable.LaunchApplication(url);
+		report.updateReport("URl launched" + url, "PASS");
+		// logger.log(LogStatus.PASS, "URl launched" + url + "<a href ='#'/>
+		// click");
+		FBLogin login = new FBLogin(driver);
+
+		// .login(Username, password);
+
+		FBProfile profile = new FBProfile(driver);
+		// report.updateReport("Profile not loaded","FAIL");
+		// logger.log(LogStatus.FAIL, "Profile not loaded");
+
+		// profile.logout();
+		Wrap();
 	}
-	
-	
-	
+
 	@AfterSuite
-	public void tearDown(){
-		report.endTest(logger);
-		report.flush();
-		System.out.println(logger.getRunStatus().toString());
-		
+	public void tearDown() {
+		report.wrapReport();
+
 	}
-	
 
 	@DataProvider(name = "test")
 
